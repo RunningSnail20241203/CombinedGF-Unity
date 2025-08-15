f87b0d6294bf1e77e339687a55204a28019ac45d
Wangzhe <xp406181@163.com>
2025-08-15 18:34:13 +0800

    增加一个VarType类型

---
+++ UnityGameFramework/Scripts/Runtime/Variable/VarType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using GameFramework;
+
+namespace UnityGameFramework.Runtime
+{
+    public sealed class VarType : Variable<Type>
+    {
+        public VarType()
+        {
+            
+        }
+
+        public static implicit operator VarType(Type value)
+        {
+            var varValue = ReferencePool.Acquire<VarType>();
+            varValue.Value = value;
+            return varValue;
+        }
+
+
+        public static implicit operator Type(VarType value)
+        {
+            return value.Value;
+        }
+    }
+}
